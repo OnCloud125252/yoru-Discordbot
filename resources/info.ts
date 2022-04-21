97f725de59c12c1202394e1d3a85e8c9d6622bd2
Anonymous-AAAA <anonymousaaaa41414141@gmail.com>
2022-04-21 16:56:35 +0800

    Update info.ts

--- resources/info.ts
+++ resources/info.ts
@@ -1,12 +1,12 @@
 export default {
     release: {
-        platform: 'Terminal',
-        version: 'Beta 1.0.0',
-        prefix: "$"
-    },
-    dev: {
         platform: 'Heroku',
         version: '1.0.0',
         prefix: "!"
+    },
+    dev: {
+        platform: 'Terminal',
+        version: 'Beta 1.0.0',
+        prefix: "$"
     }
 }
